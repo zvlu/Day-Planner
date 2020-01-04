ff980493e6a17ff7f6823d4ef18c1e497095e826
ZVLU <zvluwork@gmail.com>
2020-01-04 15:25:38 -0500

    Figured out how to display the current time and day with moment.js

---
+++ Assets/script.js
@@ -0,0 +1,18 @@
+let currentTimeDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+
+
+$( document ).ready(function() {
+
+    $('#currentDay').text(currentTimeDay);
+     console.log (currentTimeDay);
+});
+
+
+
+
+
+
+
+
+
